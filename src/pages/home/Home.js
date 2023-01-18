6cf9187febd141c3cc048c751d5375390046a6ee
Kushan Shamika <shamikakushan@gmail.com>
2023-01-18 22:59:39 +0530

    Login page design

---
+++ src/pages/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+    return (
+        <div>
+            <Typography
+                variant="h6" 
+                color="textSecondary"
+                component="h2"
+                gutterBottom
+            >
+                LQ.LK
+            </Typography>
+        </div>
+    )
+}
