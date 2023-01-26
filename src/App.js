cc554802a6414f1b7363261bb9960c2ea5935ec9
Kushan Shamika <shamikakushan@gmail.com>
2023-01-26 19:14:30 +0530

    Auth Guard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <Fragment>
@@ -14,8 +14,10 @@ function App() {
       <Router>
         <NavBar/>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
+          <Route 
+            exact path="/" 
+            element={ user ? <Home/> : <Navigate to="/login" />} />
+          <Route path="/login" element={user ? <Navigate to="/" /> : <Login/>} />
         </Routes>
       </Router>
     )}
